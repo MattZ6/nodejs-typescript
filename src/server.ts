4e6416f11a9feef1b36fe4fdacb782614ae977d8
Matheus Felipe Zanin <matt_z6@hotmail.com>
2021-07-13 15:25:43 -0300

    chore: add server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,7 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/', (_, res) => res.json({ message: '👋🌍' }))
+
+app.listen(3333, () => console.log('Running at http://localhost:3333'));
